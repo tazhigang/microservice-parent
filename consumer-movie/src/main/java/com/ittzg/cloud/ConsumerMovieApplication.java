95b0f738902918f1f1c91a5413a67bd3c306990a
tazhigang <tazhigang095@163.com>
2019-01-10 21:20:41 +0800

    服务如何注册到有用户认证的eureka上  使用@Bean

--- consumer-movie/src/main/java/com/ittzg/cloud/ConsumerMovieApplication.java
+++ consumer-movie/src/main/java/com/ittzg/cloud/ConsumerMovieApplication.java
@@ -1,9 +1,15 @@
 package com.ittzg.cloud;
 
+import com.netflix.discovery.DiscoveryClient;
+import com.sun.jersey.api.client.filter.ClientFilter;
+import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import java.util.List;
+import java.util.ArrayList;
 
 /**
  * @Author: ittzg
@@ -14,6 +20,17 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 @EnableEurekaClient
 @EnableFeignClients
 public class ConsumerMovieApplication {
+    //除去配置之外 我们可以使用@Bean实现将服务注册到用户认证的eureka上去
+    @Bean
+    public DiscoveryClient.DiscoveryClientOptionalArgs discoveryClientOptionalArgs(){
+        DiscoveryClient.DiscoveryClientOptionalArgs discoveryClientOptionalArgs =
+                new DiscoveryClient.DiscoveryClientOptionalArgs();
+        List<ClientFilter> additionalFilters = new ArrayList<ClientFilter>();
+        additionalFilters.add(new HTTPBasicAuthFilter("admin","admin123"));
+
+        discoveryClientOptionalArgs.setAdditionalFilters(additionalFilters);
+        return discoveryClientOptionalArgs;
+    }
     public static void main(String[] args) {
         SpringApplication.run(ConsumerMovieApplication.class,args);
     }
