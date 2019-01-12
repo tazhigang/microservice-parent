3028594c76d98c902715c8062eefe76f792f0c32
tazhigang <tazhigang095@163.com>
2019-01-12 23:45:54 +0800

    实现客户端的负载均衡默认模式

--- provider-user/src/main/java/com/ittzg/cloud/controller/UserController.java
+++ provider-user/src/main/java/com/ittzg/cloud/controller/UserController.java
@@ -2,10 +2,16 @@ package com.ittzg.cloud.controller;
 
 import com.ittzg.cloud.repository.entity.User;
 import com.ittzg.cloud.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @Author: ittzg
@@ -14,10 +20,14 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class UserController {
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
     @Autowired
     private UserService userService;
     @GetMapping("/{id}")
-    public User findById(@PathVariable Long id){
+    public User findById(@PathVariable Long id, HttpServletRequest request){
+        String remoteHost = request.getRemoteHost();
+        int remotePort = request.getRemotePort();
+        log.info("{}/:{}",remoteHost,remotePort);
         return userService.queryOne(id);
     }
 }
